e8759dda44f46dadb2a08485fbbd2d7993053a5f
samruddhibhangale <43806211+samruddhibhangale@users.noreply.github.com>
2023-04-19 18:13:34 +0530

    Updating script for Goerli deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,37 @@
-const { ethers, upgrades } = require("hardhat");
+const { ethers } = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  const CredentialNFT = await ethers.getContractFactory("CredentialNFT");
-  const contract = await upgrades.deployProxy(CredentialNFT);
-  await contract.deployed();
+  // Read the contract and IPFS metadata from files
+  const contract = fs.readFileSync("./artifacts/contracts/CredentialNFT.sol/CredentialNFT.json");
+  const metadata = fs.readFileSync("./metadata/nft_metadata.json");
 
-  console.log("CredentialNFT contract deployed to:", contract.address);
+  // Parse the JSON data
+  const contractData = JSON.parse(contract);
+  const metadataJson = JSON.parse(metadata);
+
+  // Connect to the Goerli testnet
+  const network = "goerli";
+  const provider = ethers.getDefaultProvider(network);
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+  // Deploy the contract
+  const CredentialNFT = await ethers.getContractFactory("CredentialNFT", signer);
+  const credentialNFT = await CredentialNFT.deploy(metadataJson.ipfsHash);
+
+  await credentialNFT.deployed();
+
+  console.log("CredentialNFT deployed to:", credentialNFT.address);
+
+  // Write the contract address to a file
+  fs.writeFileSync("./deployed_addresses.json", JSON.stringify({
+    goerli: credentialNFT.address
+  }));
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
     process.exit(1);
   });
